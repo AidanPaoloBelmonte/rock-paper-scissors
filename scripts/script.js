eac682dbef91d8921d0dd2eac8d44410e2611c99
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-07-11 13:26:23 +0800

    HTML now displays game status such as player and computer score and game results

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
 let choiceBox = document.querySelector("#choice-box");
+let roundCounter = document.querySelector("#round");
+let result = document.querySelector("#result");
+let playerScoreText = document.querySelector("#player-score");
+let computerScoreText = document.querySelector("#computer-score");
 
 let playerScore = 0;
 let computerScore = 0;
+let currentRound = 1;
+let isGameOver = false;
 
 choiceBox.addEventListener("click", (e) => {
   let choice = e.target.id;
 
+  round.textContent = `${currentRound}`;
   playRound(choice, getComputerChoice());
 });
 
@@ -21,29 +28,66 @@ function getComputerChoice() {
   }
 }
 
-function playRound(humanChoice, computerChoice) {
-  let humanChoiceF = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+function updateResult(text, noGameResult = false) {
+  if (playerScore >= 5 && !noGameResult) {
+    result.textContent = text + ` You Won the Game! Congratulations!`;
+    isGameOver = true;
+  } else if (computerScore >= 5 && !noGameResult) {
+    result.textContent = text + ` You Lost the Game! Try again next time!`;
+    isGameOver = true;
+  } else {
+    result.textContent = text;
+  }
+}
+
+function updateScores(score) {
+  switch (score) {
+    case 1:
+      playerScore++;
+      playerScoreText.textContent = playerScore;
+      break;
+    case -1:
+      computerScore++;
+      computerScoreText.textContent = computerScore;
+      break;
+  }
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (isGameOver) {
+    updateResult("The Game is over! Refresh the page to play again!", true);
+    return;
+  }
+
+  let playerChoiceF = playerChoice[0].toUpperCase() + playerChoice.slice(1);
   let computerChoiceF =
     computerChoice[0].toUpperCase() + computerChoice.slice(1);
 
-  let humanChoiceID = humanChoice[0].charCodeAt(0);
+  let playerChoiceID = playerChoice[0].charCodeAt(0);
   let computerChoiceID = computerChoice[0].charCodeAt(0);
 
-  if (Math.abs(humanChoiceID - computerChoiceID) == 3) {
-    if (humanChoiceID > computerChoiceID) {
-      console.log(`You Win! ${humanChoiceF} beats ${computerChoiceF}!`);
-      humanScore++;
+  if (Math.abs(playerChoiceID - computerChoiceID) == 3) {
+    if (playerChoiceID > computerChoiceID) {
+      updateScores(1);
+      updateResult(`You Win! ${playerChoiceF} beats ${computerChoiceF}!`);
     } else {
-      console.log(`You Lose! ${computerChoiceF} beats ${humanChoiceF}!`);
-      computerScore++;
+      updateScores(-1);
+      updateResult(`You Lose! ${computerChoiceF} beats ${playerChoiceF}!`);
     }
-  } else if (humanChoice < computerChoice) {
-    console.log(`You Win! ${humanChoiceF} beats ${computerChoiceF}!`);
-    humanScore++;
-  } else if (humanChoice > computerChoice) {
-    console.log(`You Lose! ${computerChoiceF} beats ${humanChoiceF}!`);
-    computerScore++;
+  } else if (playerChoice < computerChoice) {
+    updateScores(1);
+    updateResult(`You Win! ${playerChoiceF} beats ${computerChoiceF}!`);
+  } else if (playerChoice > computerChoice) {
+    updateScores(-1);
+    updateResult(`You Lose! ${computerChoiceF} beats ${playerChoiceF}!`);
   } else {
-    console.log(`You Tied! ${humanChoiceF} is the same as ${computerChoiceF}!`);
+    updateScores(0);
+    updateResult(
+      `You Tied! ${playerChoiceF} is the same as ${computerChoiceF}!`,
+    );
+  }
+
+  if (!isGameOver) {
+    currentRound++;
   }
 }
